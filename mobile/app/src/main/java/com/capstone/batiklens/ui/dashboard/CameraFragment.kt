a45ae2431bd16f07976720855ac730db9eed1420
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2024-11-29 01:14:14 +0700

    Implement take picture with UCrop

--- mobile/app/src/main/java/com/capstone/batiklens/ui/dashboard/CameraFragment.kt
+++ mobile/app/src/main/java/com/capstone/batiklens/ui/dashboard/CameraFragment.kt
@@ -1,19 +1,28 @@
 package com.capstone.batiklens.ui.dashboard
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.batiklens.databinding.FragmentCameraBinding
+import com.capstone.batiklens.ui.result.ResultActivity
+import com.capstone.batiklens.utils.createCustomTempFile
+import com.yalantis.ucrop.UCrop
 
 class CameraFragment : Fragment() {
 
@@ -51,13 +60,56 @@ class CameraFragment : Fragment() {
         }
 
         binding.takePicture.setOnClickListener{takePicture()}
+//        startCamera()
+    }
+
+    override fun onResume() {
+        super.onResume()
         startCamera()
+    }
+
+    private val registerUCrop = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data = UCrop.getOutput(result.data!!)
+
+            val intent = Intent(context, ResultActivity::class.java)
+            intent.putExtra("CAMERAXPIC", data.toString())
+            startActivity(intent)
+        }else if(result.resultCode == UCrop.RESULT_ERROR){
+            val uCropErr = UCrop.getError(result.data!!)
+            Log.d("UcropErr", "$uCropErr")
+        }
     }
 
 
 
     private fun takePicture() {
+        val imageCapture = imageCapture ?: return
 
+        val pictureFile = createCustomTempFile(requireContext())
+
+        val outputPicture = ImageCapture.OutputFileOptions.Builder(pictureFile).build()
+
+        imageCapture.takePicture(outputPicture, ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val uCropOption = UCrop.Options()
+                    val source = Uri.fromFile(pictureFile)
+                    val destinationFile = createCustomTempFile(requireContext(), "cropped")
+                    val destinationUri = Uri.fromFile(destinationFile)
+
+                    val uCrop = UCrop.of(source, destinationUri)
+                        .withOptions(uCropOption)
+                        .withMaxResultSize(1000,1000)
+
+                    registerUCrop.launch(uCrop.getIntent(requireContext()))
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.d("errorTakePicture", "message: $exception")
+                }
+
+            })
     }
 
     private fun startCamera() {
