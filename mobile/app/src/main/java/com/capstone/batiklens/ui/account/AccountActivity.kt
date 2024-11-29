b987fbf2f32a77405312a7363f803c403a762802
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2024-11-30 04:49:09 +0700

    add logic to HistoryActivity and fix image avatar

--- mobile/app/src/main/java/com/capstone/batiklens/ui/account/AccountActivity.kt
+++ mobile/app/src/main/java/com/capstone/batiklens/ui/account/AccountActivity.kt
@@ -1,5 +1,6 @@
 package com.capstone.batiklens.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
@@ -7,15 +8,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.batiklens.R
+import com.capstone.batiklens.databinding.ActivityAccountBinding
+import com.capstone.batiklens.ui.history.HistoryActivity
 
 class AccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Account"
 
-        setContentView(R.layout.activity_account)
+        binding = ActivityAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.setting1.setOnClickListener{
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- mobile/app/src/main/java/com/capstone/batiklens/ui/history/HistoryActivity.kt
+++ mobile/app/src/main/java/com/capstone/batiklens/ui/history/HistoryActivity.kt
@@ -1,6 +1,7 @@
 package com.capstone.batiklens.ui.history
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.batiklens.R
 
@@ -8,5 +9,18 @@ class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "History"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
